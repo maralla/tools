e3381b0fc41a2df8683f7ad7fcd5c097982d2a98
maralla <maralla.ai@gmail.com>
2018-04-18 14:13:19 +0800

    Add the number conversion tool

---
+++ numconv/src/main.rs
@@ -0,0 +1,54 @@
+#[macro_use]
+extern crate clap;
+
+use std::i32;
+use clap::{App, ArgMatches, Arg};
+
+macro_rules! die {
+    ($($arg:tt)*) => ({
+        use std::io::Write;
+        (writeln!(&mut ::std::io::stderr(), $($arg)*)).expect("stderr");
+        ::std::process::exit(1)
+    })
+}
+
+fn parse_cli() -> ArgMatches<'static> {
+    App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about("A hex/ascii/bin to decimal convertion utility.")
+        .arg(
+            Arg::with_name("value")
+                .help("The hex/ascii/bin value")
+                .required(true),
+        )
+        .get_matches()
+}
+
+fn parse_int(data: &str, radix: u32) {
+    match i32::from_str_radix(data, radix) {
+        Ok(d) => println!("{}", d),
+        Err(_) => die!("Fail to parse hex value `{}`", data),
+    }
+}
+
+fn main() {
+    let args = parse_cli();
+    match args.value_of("value") {
+        None => die!("{}", args.usage()),
+        Some(s) => {
+            if s.starts_with("0x") || s.starts_with("0X") {
+                parse_int(&s[2..], 16)
+            } else if s.starts_with("0b") || s.starts_with("0B") {
+                parse_int(&s[2..], 2)
+            } else {
+                let res = s.as_bytes()
+                    .into_iter()
+                    .map(|x| format!("{}", x))
+                    .collect::<Vec<String>>()
+                    .join(" ");
+                println!("{}", res);
+            };
+        }
+    }
+}
